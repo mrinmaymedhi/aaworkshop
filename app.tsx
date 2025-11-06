6b03660dd2119c0e3d7f66a452c8e768d192916c
MRINMAY MEDHI <41443773+mrinmaymedhi@users.noreply.github.com>
2025-11-06 21:04:56 +0530

    Add files via upload

---
+++ app.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import WorkshopLanding from './components/WorkshopLanding';
+
+const App: React.FC = () => {
+  return (
+    <WorkshopLanding />
+  );
+};
+
+export default App;
