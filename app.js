faf4a526830aec864cabb0a610ff79112bd3037a
MRINMAY MEDHI <41443773+mrinmaymedhi@users.noreply.github.com>
2026-01-09 22:11:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const importantUpdates = [
 
 const resourcePersons = [
   { name: 'Prof. Kanak Saha', inst: 'IUCAA', imageUrl: 'https://www.iucaa.in/images/Research_files/Faculty/Kanak_Saha.jpg', profileUrl: 'https://www.iucaa.in/en/faculty-research/kanak' },
-  { name: 'Dr. Biman Jyoti Medhi', inst: 'Gauhati University', imageUrl: 'https://i1.rgstatic.net/ii/profile.image/733312645595136-1551846767490_Q128/Biman-Medhi.jpg', profileUrl: 'https://www.researchgate.net/profile/Biman-Medhi' },
+  { name: 'Prof. Biman Jyoti Medhi', inst: 'Gauhati University', imageUrl: 'https://i1.rgstatic.net/ii/profile.image/733312645595136-1551846767490_Q128/Biman-Medhi.jpg', profileUrl: 'https://www.researchgate.net/profile/Biman-Medhi' },
   { name: 'Dr. Debasish Borah', inst: 'IIT Guwahati', imageUrl: '4512faculty_1080x1080.jpeg', profileUrl: 'https://sites.google.com/view/debasish-borah/home' },
   { name: 'Dr. Rupjyoti Gogoi', inst: 'Tezpur University', imageUrl: 'https://www.tezu.ernet.in/dphy/home/img/Rupjyoti_new.jpg', profileUrl: 'https://www.tezu.ernet.in/dphy/home/profiles/rup_profile.php' },
   { name: 'Dr. Sukanta Deb', inst: 'Cotton University', imageUrl: 'https://cottonuniversity.ac.in/storage/uploads/profile/fa732e5488d21b5cbb771d2a1d38cba6.jpg', profileUrl: 'https://cottonuniversity.irins.org/profile/257522' },
@@ -741,6 +741,7 @@ document.addEventListener('DOMContentLoaded', () => {
   startCTACountdown(workshopStart);
   renderUpdatesTicker(); // 🔔 NEW
 });
+
 
 
 
